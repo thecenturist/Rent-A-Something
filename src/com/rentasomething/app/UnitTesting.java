2d1a0c4a84355249bd0c042420a71130ea8a23b4
AJ <a.anjolaoluwa@yahoo.com>
2024-03-18 22:36:30 -0700

    completed unit test for Person class

--- src/com/rentasomething/app/UnitTesting.java
+++ src/com/rentasomething/app/UnitTesting.java
@@ -18,10 +18,19 @@ import org.junit.jupiter.api.Test;
 class UnitTesting {
 	@Test
 	void testPerson() {
-		Person p1 = new Person("Jamie", "Finnicky", 983022304);
+		Person p1 = new Person("Jamie", "Finnicky", 983022304L);
 		assertEquals(983022304, p1.getSSN());
 		assertEquals("Finnicky", p1.getLastName());
 		assertEquals("Jamie", p1.getFirstName());
 		assertEquals("Jamie Finnicky", p1.getFullName());
+		p1.setFirstName("Amie");
+		assertNotEquals("Jamie Finnicky 983022304", p1.toString());
+		p1.setLastName("Johnston");
+		p1.setSSN(9998883332L);
+		assertEquals(9998883332L, p1.getSSN());
+		assertEquals("Johnston", p1.getLastName());
+		assertEquals("Amie", p1.getFirstName());
+		assertEquals("Amie Johnston", p1.getFullName());
+		assertEquals("Amie Johnston 9998883332", p1.toString());
 	}
 }
