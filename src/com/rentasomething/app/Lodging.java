9ab5efa01c38c4a7cee8599c9a20933beafaa1c2
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-03-21 20:09:23 -0700

    Create Lodging.java

---
+++ src/com/rentasomething/app/Lodging.java
@@ -0,0 +1,72 @@
+
+package com.rentasomething.app;
+
+import java.nio.file.ProviderNotFoundException;
+
+/**
+ * Lead Author(s):
+ * @author Kaitlyn Nguy
+ * 
+ * References:
+ * Version/date: March-21-2024
+ * 
+ * Responsibilities of class:
+ * Model a Lodging object which is a Product
+ */
+
+// A Lodging is-a Product
+public class Lodging extends Product
+{
+        // A Lodging has-a renter name
+        private String renterName;
+        // A Lodging has-a max capacity
+        private int maxCapacity;
+
+        public Lodging(String renterName, int maxCapacity)
+        {
+            super(0, 0);
+            this.renterName = renterName;
+            this.maxCapacity = maxCapacity;
+        }
+
+        public Lodging(String renterName, int maxCapacity, double rent, double discount)
+        {
+            super(rent, discount);
+            this.renterName = renterName;
+            this.maxCapacity = maxCapacity;
+        }
+        
+        /**
+         * Set the renter name of the Lodging
+         * @param manufacturer
+         */
+        public void setRenterName(String renterName) {
+            this.renterName = renterName;
+        }
+        
+        /**
+         * Set the max capacity of the Lodging
+         * @param brand
+         */
+        public void setMaxCapacity(int maxCapacity) {
+            this.maxCapacity = maxCapacity;
+        }
+        
+        /**
+         * Return the renter name of the Lodging
+         * @return String
+         */
+        public String getRenterName() {
+            return this.renterName;
+        };
+        
+        /**
+         * Return the max capacity of the Lodging
+         * @return int
+         */
+        public int getMaxCapacity() {
+            return this.maxCapacity;
+        } 
+
+}
+
