4488610970b9553662a8a8fee0515928065b37a7
AJ <a.anjolaoluwa@yahoo.com>
2024-03-22 16:19:08 -0700

    Added file header

--- src/com/rentasomething/app/Rental.java
+++ src/com/rentasomething/app/Rental.java
@@ -1,5 +1,16 @@
 package com.rentasomething.app;
 
+/**
+ * Lead Author(s):
+ * @author Anjolaoluwa Akinremi
+ * 
+ * References:
+ * Version/date: March-9-2024
+ * 
+ * Responsibilities of interface:
+ * Enforce requirements for a Rental object
+ */
+
 public interface Rental
 {
 	double getRentAmount();
