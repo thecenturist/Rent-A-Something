ed80e14268ff67b28a29e5c7f7784a855ccc0ee8
AJ <a.anjolaoluwa@yahoo.com>
2024-05-06 21:35:41 -0700

    Added submission logic and validation

--- src/com/rentasomething/app/CreatePersonPanel.java
+++ src/com/rentasomething/app/CreatePersonPanel.java
@@ -3,16 +3,32 @@ package com.rentasomething.app;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+/**
+ * Lead Author(s):
+ * 
+ * @author Kaitlyn Nguy
+ * @author Anjolaoluwa Akinremi
+ * 
+ *         References:
+ *         Version/date: April-28-2024
+ * 
+ *         Responsibilities of class:
+ *         This will be a JPanel that deals with the creation of a new Person in the application, including saving the details of the Person created to a database.
+ */
+
 public class CreatePersonPanel extends JPanel
 {
 	public CreatePersonPanel() {
 		this.setLayout(new BorderLayout());
+		
+		// TODO: Make visual components closer together, and text fields smaller to fit only a name.
 		
 		JPanel northPanel = new JPanel();
 		JLabel createTitle = new JLabel("CREATE NEW PERSON");
@@ -24,15 +40,18 @@ public class CreatePersonPanel extends JPanel
 
 		JLabel firstName = new JLabel("     First Name: ");
 		centerPanel.add(firstName);
-		centerPanel.add(new JTextField());
+		JTextField firstNameField = new JTextField();
+		centerPanel.add(firstNameField);
 
 		JLabel lastName = new JLabel("     Last Name: ");
 		centerPanel.add(lastName);
-		centerPanel.add(new JTextField());
+		JTextField lastNameField = new JTextField();
+		centerPanel.add(lastNameField);
 
 		JLabel ssn = new JLabel("     SSN: ");
 		centerPanel.add(ssn);
-		centerPanel.add(new JTextField());
+		JTextField ssnField = new JTextField();
+		centerPanel.add(ssnField);
 		
 		this.add(centerPanel, BorderLayout.CENTER);
 		
@@ -41,6 +60,21 @@ public class CreatePersonPanel extends JPanel
 		createButton.setPreferredSize(new Dimension(100, 50));
 		southPanel.add(createButton);
 		this.add(southPanel, BorderLayout.SOUTH);
+		
+		createButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				if (firstNameField.getText().isEmpty() ||lastNameField.getText().isEmpty() || ssnField.getText().isEmpty()) {
+					new ErrorDialog("Fields cannot be empty");
+				} else {
+					// TODO: Properly validate that SSN is only numbers, and if not, throw error pop up
+					Person person = new Person(firstNameField.getText(), lastNameField.getText(), Long.parseLong(ssnField.getText()));
+					// TODO: Save person to database (csv file)
+					System.out.println(person.toString());	
+				}	
+			}
+		});
 		
 	}
 }
