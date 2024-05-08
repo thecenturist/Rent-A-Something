7c093a6e511021cec5c1b9cbfd6f708f85a43624
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-07 21:56:01 -0700

    Updates and lists all Person data

--- src/com/rentasomething/app/ViewPeoplePanel.java
+++ src/com/rentasomething/app/ViewPeoplePanel.java
@@ -1,28 +1,64 @@
 package com.rentasomething.app;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import javax.swing.JButton;
+import java.awt.Component;
+import java.util.ArrayList;
+
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ViewPeoplePanel extends JPanel
 {
-	public ViewPeoplePanel() {
+
+	private static ArrayList<Person> displayPersons = new ArrayList<>();
+
+	public ViewPeoplePanel()
+	{
 		this.setLayout(new BorderLayout());
-		
-		// TODO: Write GUI code to display all Persons in the database, the rows should include buttons to edit and delete the person. Preferably, when the edit button is pressed, the firstname, lastname, and ssn labels should turn into textfields with the content present for editing.
+
+		// TODO: Write GUI code to display all Persons in the database, the rows
+		// should include buttons to edit and delete the person. Preferably,
+		// when the edit button is pressed, the firstname, lastname, and ssn
+		// labels should turn into textfields with the content present for
+		// editing.
 		JPanel northPanel = new JPanel();
 		JLabel createTitle = new JLabel("VIEW PEOPLE");
 		northPanel.add(createTitle);
 		this.add(northPanel, BorderLayout.NORTH);
 
 		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new GridLayout(3, 2, 5, 18));
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
-		
-		
+		if (displayPersons.isEmpty() == false)
+		{
+			for (int person = 0; person < displayPersons.size(); person++)
+			{
+				JLabel personLabel = new JLabel(
+						displayPersons.get(person).toString());
+				personLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+				centerPanel.add(personLabel);
+			}
+		}
+		else
+		{
+			JLabel noPersons = new JLabel("No persons at the time");
+			centerPanel.add(noPersons);
+		}
+
 		this.add(centerPanel, BorderLayout.CENTER);
+	}
+
+	public static void updatePersons()
+	{
+		try
+		{
+			displayPersons = CreatePersonPanel.getAllPersons();
+		}
+		catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
