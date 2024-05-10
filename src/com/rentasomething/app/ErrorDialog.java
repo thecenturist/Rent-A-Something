ebb92897636fb8d5977aa35aaf0be36b99295833
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-10 16:25:59 -0700

    Added in methods to ErrorDialog.java

--- src/com/rentasomething/app/ErrorDialog.java
+++ src/com/rentasomething/app/ErrorDialog.java
@@ -1,6 +1,5 @@
 package com.rentasomething.app;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
@@ -10,17 +9,67 @@ import javax.swing.SwingConstants;
 
 public class ErrorDialog extends JDialog
 {
-	// TODO: Make it so the main window cannot be returned to unless the dialog is closed
-	public ErrorDialog(String message) {
+	// TODO: Make it so the main window cannot be returned to unless the dialog
+	// is closed
+	public ErrorDialog(String message)
+	{
 		this.setTitle("ERROR");
-		this.setSize(400,100);
+		this.setSize(400, 100);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+		this.setLocation(dim.width / 2 - this.getSize().width / 2,
+				dim.height / 2 - this.getSize().height / 2);
 		JLabel jl = new JLabel(message);
 		jl.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(jl);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+	}
+
+	public static boolean containsNumbers(String str)
+	{
+		for (int checkChar = 0; checkChar < str.length(); checkChar++)
+		{
+			for (int number = 0; number < 10; number++)
+			{
+				if (str.charAt(checkChar) == (char) number)
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static boolean containsLetters(String str)
+	{
+		for (int checkChar = 0; checkChar < str.length(); checkChar++)
+		{
+			// 57 is ASCII value for 9
+			// 48 is ASCII value for 0
+			if (str.charAt(checkChar) > 57 || str.charAt(checkChar) < 48)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean isDecimal(String str)
+	{
+		for (int checkChar = 0; checkChar < str.length(); checkChar++)
+		{
+			// 57 is ASCII value for 9
+			// 48 is ASCII value for 0
+			// 46 is ASCII value for .
+			if (str.charAt(checkChar) > 57 || str.charAt(checkChar) < 48)
+			{
+				if (str.charAt(checkChar) != 46)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 }
