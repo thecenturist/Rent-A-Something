a312f1c2a969377c2336e45df833ff1b55534803
AJ <a.anjolaoluwa@yahoo.com>
2024-05-10 17:45:47 -0700

    Accept param

--- src/com/rentasomething/app/ViewProductPanel.java
+++ src/com/rentasomething/app/ViewProductPanel.java
@@ -1,13 +1,14 @@
 package com.rentasomething.app;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 // TODO: Write GUI code to display all Products
 public class ViewProductPanel extends JPanel
 {
-	public ViewProductPanel() {
+	public ViewProductPanel(ArrayList displayProducts) {
 		this.setLayout(new BorderLayout());
 	}
 }
