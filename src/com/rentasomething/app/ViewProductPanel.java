d738596c9b182bce0b14aa8809f57b7306106fa1
AJ <a.anjolaoluwa@yahoo.com>
2024-05-10 17:20:47 -0700

    Added view product file

---
+++ src/com/rentasomething/app/ViewProductPanel.java
@@ -0,0 +1,13 @@
+package com.rentasomething.app;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+// TODO: Write GUI code to display all Products
+public class ViewProductPanel extends JPanel
+{
+	public ViewProductPanel() {
+		this.setLayout(new BorderLayout());
+	}
+}
