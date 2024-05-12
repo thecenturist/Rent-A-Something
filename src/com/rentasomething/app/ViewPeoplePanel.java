5b53157ac49d428612eaae597a986651b6d801b6
AJ <a.anjolaoluwa@yahoo.com>
2024-05-12 09:13:31 -0700

    Added Delete Person dialog within View People file

--- src/com/rentasomething/app/ViewPeoplePanel.java
+++ src/com/rentasomething/app/ViewPeoplePanel.java
@@ -2,15 +2,23 @@ package com.rentasomething.app;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class ViewPeoplePanel extends JPanel
 {
@@ -27,8 +35,78 @@ public class ViewPeoplePanel extends JPanel
 		JPanel northPanel = new JPanel();
 		JLabel createTitle = new JLabel("VIEW PEOPLE");
 		northPanel.add(createTitle);
-		northPanel.add(new JButton("DELETE"));
+		JButton deleteButton = new JButton("DELETE");
+		northPanel.add(deleteButton);
 		this.add(northPanel, BorderLayout.NORTH);
+		
+		deleteButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+//				new DeletePersonDialog(displayPersons);
+				JDialog deletePersonDialog = new JDialog();
+				deletePersonDialog.setTitle("Delete Person by SSN");
+				deletePersonDialog.setSize(400, 100);
+				deletePersonDialog.setLayout(new BoxLayout(deletePersonDialog.getContentPane(), BoxLayout.Y_AXIS));
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+				deletePersonDialog.setLocation(dim.width / 2 - deletePersonDialog.getSize().width / 2,
+						dim.height / 2 - deletePersonDialog.getSize().height / 2);
+				
+				JPanel subPanel = new JPanel();
+				subPanel.setSize(new Dimension(300, 100));
+				JLabel jl = new JLabel("Enter SSN:");
+				jl.setHorizontalAlignment(SwingConstants.CENTER);
+				JTextField ssnField = new JTextField();
+				subPanel.add(jl);
+				subPanel.add(ssnField);
+				JButton deleteSubmitButton = new JButton("Delete");
+				ssnField.setSize(new Dimension(100, 50));
+				
+				deletePersonDialog.add(jl);
+				deletePersonDialog.add(ssnField);
+//				deletePersonDialog.add(subPanel);
+				deletePersonDialog.add(deleteSubmitButton);
+				deletePersonDialog.setVisible(true);
+				deletePersonDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				
+				deleteSubmitButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						Long ssn;
+						try {
+							if (!displayPersons.isEmpty()) {
+								ssn = Long.parseLong(ssnField.getText());
+								for (Person p : displayPersons) {
+									if (p.getSSN() == ssn) {
+										System.out.println("Deleting: " + ssn);
+										displayPersons.remove(p);
+										deletePersonDialog.dispose();
+										revalidate();
+										repaint();
+									} else {
+										new ErrorDialog("There is no person matching that SSN");
+									}
+									
+								}
+							} else {
+								new ErrorDialog("There are no persons at this time");
+							}
+							
+							
+						} catch (NumberFormatException ex) {
+							new ErrorDialog("Unable to format SSN");
+						}
+						
+						
+						
+					}
+					
+				});
+			}
+			
+		});
 
 		JPanel centerPanel = new JPanel();
 		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
