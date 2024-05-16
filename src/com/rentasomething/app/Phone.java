ec94ec228aaacc9278b5baa67b7f1f4450ad7916
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 16:19:07 -0700

    Updated Phone to have Person in parameter

--- src/com/rentasomething/app/Phone.java
+++ src/com/rentasomething/app/Phone.java
@@ -2,91 +2,98 @@ package com.rentasomething.app;
 
 /**
  * Lead Author(s):
+ * 
  * @author Kaitlyn Nguy
  * 
- * References:
- * Version/date: March-21-2024
+ *         References:
+ *         Version/date: March-21-2024
  * 
- * Responsibilities of class:
- * Model a Phone object which is a type of Electronic
+ *         Responsibilities of class:
+ *         Model a Phone object which is a type of Electronic
  */
 
 // A Phone is-a Electronic
 public class Phone extends Electronic
 {
-        // A Phone has-a mass
-        private int mass;
-        // A Phone has-a CPU
-        private String CPU;
-        // A Phone has-a numberOfCameras
-        private int numberOfCameras;
-        
-        public Phone(String manufacturer, String brand, String model)
-        {
-            super(manufacturer, brand, model);
-        }
+	// A Phone has-a mass
+	private int mass;
+	// A Phone has-a CPU
+	private String CPU;
+	// A Phone has-a numberOfCameras
+	private int numberOfCameras;
 
-        public Phone(String manufacturer, String brand, String model, int mass, String CPU, int numberOfCameras)
-        {
-            super(manufacturer, brand, model);
-            this.mass = mass;
-            this.CPU = CPU;
-            this.numberOfCameras = numberOfCameras;
-        }
+	public Phone(Person person, String manufacturer, String brand, String model)
+	{
+		super(person, manufacturer, brand, model);
+	}
 
-        /**
-         * Set the mass of the Phone
-         * @param mass
-         */
-        public void setMass(int mass) 
-        {
-            this.mass = mass;
-        }
-        
-        /**
-         * Set the CPU of the Phone
-         * @param CPU
-         */
-        public void setCPU(String CPU) 
-        {
-            this.CPU = CPU;
-        }
+	public Phone(Person person, String manufacturer, String brand, String model,
+			int mass, String CPU, int numberOfCameras)
+	{
+		super(person, manufacturer, brand, model);
+		this.mass = mass;
+		this.CPU = CPU;
+		this.numberOfCameras = numberOfCameras;
+	}
 
-        /**
-         * Set the number of cameras of the Phone
-         * @param numberOfCameras
-         */
-        public void setNumberOfCameras(int numberOfCameras) 
-        {
-            this.numberOfCameras = numberOfCameras;
-        }
-        
-        /**
-         * Return the mass of the Phone
-         * @return int
-         */
-        public int getMass() 
-        {
-            return this.mass;
-        };
-        
-        /**
-         * Return the CPU of the Phone
-         * @return String
-         */
-        public String getCPU() 
-        {
-            return this.CPU;
-        }
+	/**
+	 * Set the mass of the Phone
+	 * 
+	 * @param mass
+	 */
+	public void setMass(int mass)
+	{
+		this.mass = mass;
+	}
 
-        /**
-         * Return the number of cameras of the Phone
-         * @return int
-         */
-        public int getNumberOfCameras() 
-        {
-            return this.numberOfCameras;
-        }
+	/**
+	 * Set the CPU of the Phone
+	 * 
+	 * @param CPU
+	 */
+	public void setCPU(String CPU)
+	{
+		this.CPU = CPU;
+	}
+
+	/**
+	 * Set the number of cameras of the Phone
+	 * 
+	 * @param numberOfCameras
+	 */
+	public void setNumberOfCameras(int numberOfCameras)
+	{
+		this.numberOfCameras = numberOfCameras;
+	}
+
+	/**
+	 * Return the mass of the Phone
+	 * 
+	 * @return int
+	 */
+	public int getMass()
+	{
+		return this.mass;
+	};
+
+	/**
+	 * Return the CPU of the Phone
+	 * 
+	 * @return String
+	 */
+	public String getCPU()
+	{
+		return this.CPU;
+	}
+
+	/**
+	 * Return the number of cameras of the Phone
+	 * 
+	 * @return int
+	 */
+	public int getNumberOfCameras()
+	{
+		return this.numberOfCameras;
+	}
 
 }
-
