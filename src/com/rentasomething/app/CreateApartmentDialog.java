701f38c4fdfd156f3534537fd07aad91c55f799c
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 00:33:06 -0700

    Can create objects now
    Subject to error....

--- src/com/rentasomething/app/CreateApartmentDialog.java
+++ src/com/rentasomething/app/CreateApartmentDialog.java
@@ -1,25 +1,134 @@
 package com.rentasomething.app;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * Lead Author(s):
+ * 
+ * @author Kaitlyn Nguy
+ * @author Anjolaoluwa Akinremi
+ * 
+ *         References:
+ *         Version/date: May-15-2024
+ * 
+ *         Responsibilities of class:
+ *         Creates apartment object
+ */
 
 public class CreateApartmentDialog extends JDialog
 {
-	public CreateApartmentDialog() {
+	public CreateApartmentDialog()
+	{
+		JPanel bottomPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(7, 2, 5, 18));
+
 		this.setTitle("Create Apartment");
-		this.setSize(400, 100);
+		this.setSize(1200, 600);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width / 2 - this.getSize().width / 2,
 				dim.height / 2 - this.getSize().height / 2);
-		JLabel jl = new JLabel("Apartment creation");
-		jl.setHorizontalAlignment(SwingConstants.CENTER);
-		this.add(jl);
+		this.setLayout(new BorderLayout());
+
+		JLabel rnLabel = new JLabel("   Renter Name:");
+		JComboBox rnComboBox = new JComboBox(Database.namesArray());
+		centerPanel.add(rnLabel);
+		centerPanel.add(rnComboBox);
+
+		JLabel mcLabel = new JLabel("   Max Capacity:");
+		JTextField mcTextField = new JTextField();
+		centerPanel.add(mcLabel);
+		centerPanel.add(mcTextField);
+
+		JLabel anLabel = new JLabel("   Apartment Number:");
+		JTextField anTextField = new JTextField();
+		centerPanel.add(anLabel);
+		centerPanel.add(anTextField);
+
+		JLabel apLabel = new JLabel("   Apartment Price:");
+		JTextField apTextField = new JTextField();
+		centerPanel.add(apLabel);
+		centerPanel.add(apTextField);
+
+		JLabel nBeLabel = new JLabel("   Number of Bedrooms:");
+		JTextField nBeTextField = new JTextField();
+		centerPanel.add(nBeLabel);
+		centerPanel.add(nBeTextField);
+
+		JLabel nBaLabel = new JLabel("   Number of Bathrooms:");
+		JTextField nBaTextField = new JTextField();
+		centerPanel.add(nBaLabel);
+		centerPanel.add(nBaTextField);
+
+		JButton createButton = new JButton("Create");
+		bottomPanel.add(createButton);
+
+		this.add(bottomPanel, BorderLayout.SOUTH);
+		this.add(centerPanel, BorderLayout.CENTER);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+		createButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				if (mcTextField.getText().isEmpty()
+						|| anTextField.getText().isEmpty()
+						|| apTextField.getText().isEmpty()
+						|| nBeTextField.getText().isEmpty()
+						|| nBaTextField.getText().isEmpty())
+				{
+					new ErrorDialog("Fields cannot be empty");
+				}
+				else if (ErrorDialog
+						.containsLetters(mcTextField.getText()) == true
+						|| ErrorDialog
+								.containsLetters(anTextField.getText()) == true
+						|| ErrorDialog
+								.containsLetters(apTextField.getText()) == true)
+				{
+					new ErrorDialog(
+							"Max capacity, apartment number, and apartment price must be integers");
+				}
+				else if (ErrorDialog.isDecimal(nBeTextField.getText()) == false
+						|| ErrorDialog
+								.isDecimal(nBaTextField.getText()) == false)
+				{
+					new ErrorDialog(
+							"Number of bedrooms and bathrooms must be numbers");
+				}
+				else
+				{
+					// TODO: Properly validate that SSN is only numbers, and if
+					// not, throw error pop up
+					// TODO: Save person to database (csv file)
+					Hashtable<String, Person> personIdentification = Database
+							.getNames();
+					Apartment apartment = new Apartment(
+							personIdentification
+									.get(rnComboBox.getSelectedItem()),
+							Integer.parseInt(mcTextField.getText()),
+							Integer.parseInt(anTextField.getText()),
+							Integer.parseInt(apTextField.getText()),
+							Long.parseLong(nBeTextField.getText()),
+							Long.parseLong(nBaTextField.getText()));
+				}
+			}
+		});
+
 	}
 }
