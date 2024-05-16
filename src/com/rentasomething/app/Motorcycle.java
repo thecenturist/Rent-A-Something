a250901393025c8e6c7fb9859803bbb073a21cc2
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 16:16:36 -0700

    Can create Motorcycle objects now

--- src/com/rentasomething/app/Motorcycle.java
+++ src/com/rentasomething/app/Motorcycle.java
@@ -2,67 +2,100 @@ package com.rentasomething.app;
 
 /**
  * Lead Author(s):
+ * 
  * @author Kaitlyn Nguy
  * 
- * References:
- * Version/date: March-18-2024
+ *         References:
+ *         Version/date: March-18-2024
  * 
- * Responsibilities of class:
- * Model a Motorcycle object which is a type of Vehicle
+ *         Responsibilities of class:
+ *         Model a Motorcycle object which is a type of Vehicle
  */
 
 // A Motorcycle is-a Vehicle
 public class Motorcycle extends Vehicle
 {
-        // A Motorcycle has-a weight, in kilograms
-        private double weight;
-        // A Motorcycle has-a powerOutput, in CC
-        private double powerOutput;
-        
-        public Motorcycle(String manufacturer, String model, int year)
-        {
-            super(manufacturer, model, year);
-        }
-        
-        public Motorcycle(String manufacturer, String model, int year, int weight, int powerOutput)
-        {
-            super(manufacturer, model, year);
-            this.weight = weight;
-            this.powerOutput = powerOutput;
-        }
-        
-        /**
-         * Set the weight (kg) of the Motorcycle
-         * @param weight
-         */
-        public void setWeight(double weight) {
-            // assumes weight to new weight
-            this.weight = weight;
-        }
-    
-        /**
-         * Return the weight (kg) of the Motorcycle
-         * @return double
-         */
-        public double getWeight() {
-            // returns the result
-            return this.weight;
-        }
-    
-        /**
-         * Set the power output (cc) of the Motorcycle
-         * @param powerOutput
-         */
-        public void setPowerOutput(double powerOutput) {
-            this.powerOutput = powerOutput;
-        }
-        
-        /**
-         * Return the power output (cc) of the Motorcycle
-         * @return double
-         */
-        public double powerOutput() {
-            return this.powerOutput;
-        }
-}
+	// A Motorcycle has-a weight, in kilograms
+	private long weight;
+	// A Motorcycle has-a powerOutput, in CC
+	private long powerOutput;
+	private Person renter;
 
+	public Motorcycle(String manufacturer, String model, int year,
+			Person person)
+	{
+		super(manufacturer, model, year, person);
+	}
+
+	public Motorcycle(String manufacturer, String model, int year,
+			Person person, long weight, long powerOutput)
+	{
+		super(manufacturer, model, year, person);
+		this.weight = weight;
+		this.powerOutput = powerOutput;
+	}
+
+	/**
+	 * Set the weight (kg) of the Motorcycle
+	 * 
+	 * @param weight
+	 */
+	public void setWeight(long weight)
+	{
+		// assumes weight to new weight
+		this.weight = weight;
+	}
+
+	/**
+	 * Return the weight (kg) of the Motorcycle
+	 * 
+	 * @return double
+	 */
+	public long getWeight()
+	{
+		// returns the result
+		return this.weight;
+	}
+
+	/**
+	 * Set the power output (cc) of the Motorcycle
+	 * 
+	 * @param powerOutput
+	 */
+	public void setPowerOutput(long powerOutput)
+	{
+		this.powerOutput = powerOutput;
+	}
+
+	/**
+	 * Return the power output (cc) of the Motorcycle
+	 * 
+	 * @return double
+	 */
+	public long powerOutput()
+	{
+		return this.powerOutput;
+	}
+
+	/**
+	 * Set the renter of the Vehicle
+	 * 
+	 * @param renter
+	 */
+	@Override
+	public void setRenter(Person renter)
+	{
+		this.renter = renter;
+	}
+
+	/**
+	 * Return the renter of the Vehicle
+	 * 
+	 * @return renter
+	 */
+	@Override
+	public Person getRenter()
+	{
+		return this.renter;
+	}
+}
