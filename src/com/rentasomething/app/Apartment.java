50115e30d5f632c2c9c138f8f6a154b8e0c0bde7
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 00:36:37 -0700

    Updated to match superclass

--- src/com/rentasomething/app/Apartment.java
+++ src/com/rentasomething/app/Apartment.java
@@ -2,113 +2,121 @@ package com.rentasomething.app;
 
 /**
  * Lead Author(s):
+ * 
  * @author Kaitlyn Nguy
- * @author Anjolaoluwa Akinremi
  * 
- * References:
- * Version/date: March-21-2024
+ *         References:
+ *         Version/date: March-21-2024
  * 
- * Responsibilities of class:
- * Model an Apartment object which is a Lodging
+ *         Responsibilities of class:
+ *         Model an Apartment object which is a Lodging
  */
 
 // An Apartment is-a Lodging
 public class Apartment extends Lodging
 {
-        // An Apartment has-an apartment number
-        private int apartmentNumber;
-        // An Apartment has-a price
-        private int apartmentPrice;
-        // An Apartment has-a number of bedrooms
-        private int numberOfBedrooms;
-        // An Apartment has-a number of bathrooms
-        private int numberOfBathrooms;
+	// An Apartment has-an apartment number
+	private int apartmentNumber;
+	// An Apartment has-a price
+	private int apartmentPrice;
+	// An Apartment has-a number of bedrooms
+	private long numberOfBedrooms;
+	// An Apartment has-a number of bathrooms
+	private long numberOfBathrooms;
 
-        public Apartment(Person renter, int maxCapacity)
-        {
-            super(renter, maxCapacity);
-        }
+	public Apartment(Person renter, int maxCapacity)
+	{
+		super(renter, maxCapacity);
+	}
 
-        public Apartment(Person renter, int maxCapacity, int apartmentNumber, int apartmentPrice, int numberOfBedrooms, int numberOfBathrooms)
-        {
-            super(renter, maxCapacity);
-            this.apartmentNumber = apartmentNumber;
-            this.apartmentPrice = apartmentPrice;
-            this.numberOfBedrooms = numberOfBedrooms;
-            this.numberOfBathrooms = numberOfBathrooms;
-        }
-        
-        /**
-         * Set the apartment number of the Apartment
-         * @param apartmentNumber
-         */
-        public void setApartmentNumber(int apartmentNumber) 
-        {
-            this.apartmentNumber = apartmentNumber;
-        }
-        
-        /**
-         * Set the price of the Apartment
-         * @param apartmentPrice
-         */
-        public void setApartmentPrice(int apartmentPrice) 
-        {
-            this.apartmentPrice = apartmentPrice;
-        }
+	public Apartment(Person renter, int maxCapacity, int apartmentNumber,
+			int apartmentPrice, long numberOfBedrooms, long numberOfBathrooms)
+	{
+		super(renter, maxCapacity);
+		this.apartmentNumber = apartmentNumber;
+		this.apartmentPrice = apartmentPrice;
+		this.numberOfBedrooms = numberOfBedrooms;
+		this.numberOfBathrooms = numberOfBathrooms;
+	}
 
-        /**
-         * Set the number of bedrooms of the Apartment
-         * @param numberOfBedrooms
-         */
-        public void setNumberOfBedrooms(int numberOfBedrooms)
-        {
-            this.numberOfBedrooms = numberOfBedrooms;
-        }
+	/**
+	 * Set the apartment number of the Apartment
+	 * 
+	 * @param apartmentNumber
+	 */
+	public void setApartmentNumber(int apartmentNumber)
+	{
+		this.apartmentNumber = apartmentNumber;
+	}
 
-        /**
-         * Set the number of bathrooms of the Apartment
-         * @param numberOfBathrooms
-         */
-        public void setNumberOfBathrooms(int numberOfBathrooms)
-        {
-            this.numberOfBathrooms = numberOfBathrooms;
-        }
-        
-        /**
-         * Return the apartment number of the Apartment
-         * @return int
-         */
-        public int getApartmentNumber() 
-        {
-            return this.apartmentNumber;
-        };
-        
-        /**
-         * Return the price of the Apartment
-         * @return int
-         */
-        public int getApartmentPrice() 
-        {
-            return this.apartmentPrice;
-        } 
+	/**
+	 * Set the price of the Apartment
+	 * 
+	 * @param apartmentPrice
+	 */
+	public void setApartmentPrice(int apartmentPrice)
+	{
+		this.apartmentPrice = apartmentPrice;
+	}
 
-        /**
-         * Return the number of bedrooms of the Apartment
-         * @return int
-         */
-        public int getNumberOfBedrooms() 
-        {
-            return this.numberOfBedrooms;
-        } 
+	/**
+	 * Set the number of bedrooms of the Apartment
+	 * 
+	 * @param numberOfBedrooms
+	 */
+	public void setNumberOfBedrooms(int numberOfBedrooms)
+	{
+		this.numberOfBedrooms = numberOfBedrooms;
+	}
 
-        /**
-         * Return the number of bathrooms of the Apartment
-         * @return int
-         */
-        public int getNumberOfBathrooms() 
-        {
-            return this.numberOfBathrooms;
-        } 
+	/**
+	 * Set the number of bathrooms of the Apartment
+	 * 
+	 * @param numberOfBathrooms
+	 */
+	public void setNumberOfBathrooms(int numberOfBathrooms)
+	{
+		this.numberOfBathrooms = numberOfBathrooms;
+	}
+
+	/**
+	 * Return the apartment number of the Apartment
+	 * 
+	 * @return int
+	 */
+	public int getApartmentNumber()
+	{
+		return this.apartmentNumber;
+	};
+
+	/**
+	 * Return the price of the Apartment
+	 * 
+	 * @return int
+	 */
+	public int getApartmentPrice()
+	{
+		return this.apartmentPrice;
+	}
+
+	/**
+	 * Return the number of bedrooms of the Apartment
+	 * 
+	 * @return int
+	 */
+	public long getNumberOfBedrooms()
+	{
+		return this.numberOfBedrooms;
+	}
+
+	/**
+	 * Return the number of bathrooms of the Apartment
+	 * 
+	 * @return int
+	 */
+	public long getNumberOfBathrooms()
+	{
+		return this.numberOfBathrooms;
+	}
 
 }
-
