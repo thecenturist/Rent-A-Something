5be9a40bb76b69c77685acd39e150970bc965d1d
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 16:12:25 -0700

    Updated Car to have Person in parameter

--- src/com/rentasomething/app/Car.java
+++ src/com/rentasomething/app/Car.java
@@ -2,43 +2,51 @@ package com.rentasomething.app;
 
 /**
  * Lead Author(s):
+ * 
  * @author Anjolaoluwa Akinremi
  * 
- * References:
- * Version/date: March-9-2024
+ *         References:
+ *         Version/date: March-9-2024
  * 
- * Responsibilities of class:
- * Model a Car object which is a type of Vehicle
+ *         Responsibilities of class:
+ *         Model a Car object which is a type of Vehicle
  */
 
 // A Car is-a Vehicle
 public class Car extends Vehicle
 {
-	private double odometerReading; // A Car has-a odometerReading
-	
-	Car(String manufacturer, String model, int year, double mileage)
+	private long odometerReading; // A Car has-a odometerReading
+
+	Car(String manufacturer, String model, int year, Person person,
+			long mileage)
 	{
-		super(manufacturer, model, year);
+		super(manufacturer, model, year, person);
 		this.odometerReading = mileage;
 	}
-	
-	Car(Vehicle vehicle, double mileage){
-		this(vehicle.getManufacturer(), vehicle.getModel(), vehicle.getYear(), mileage);
+
+	Car(Vehicle vehicle, long mileage)
+	{
+		this(vehicle.getManufacturer(), vehicle.getModel(), vehicle.getYear(),
+				vehicle.getRenter(), mileage);
 	}
-	
+
 	/**
 	 * Return the value of the odometer reading
+	 * 
 	 * @return double
 	 */
-	public double getOdometerReading() {
+	public long getOdometerReading()
+	{
 		return this.odometerReading;
 	}
-	
+
 	/**
 	 * Set the odometer reading
+	 * 
 	 * @param mileage
 	 */
-	public void setOdometerReading(double mileage) {
+	public void setOdometerReading(long mileage)
+	{
 		this.odometerReading = mileage;
 	}
 }
