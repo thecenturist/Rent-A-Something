3fe516d8a8fb0c6dda0b11800a3766c7c6892a86
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 16:10:44 -0700

    Updated Boat to have Person in parameter

--- src/com/rentasomething/app/Boat.java
+++ src/com/rentasomething/app/Boat.java
@@ -2,62 +2,73 @@ package com.rentasomething.app;
 
 /**
  * Lead Author(s):
+ * 
  * @author Anjolaoluwa Akinremi
  * 
- * References:
- * Version/date: March-9-2024
+ *         References:
+ *         Version/date: March-9-2024
  * 
- * Responsibilities of class:
- * Model a Boat object which is a type of Vehicle
+ *         Responsibilities of class:
+ *         Model a Boat object which is a type of Vehicle
  */
 
 // A Boat is-a Vehicle
 public class Boat extends Vehicle
 {
-	private double length; // A Boat has-a length, in meters
-	private double topSpeed; // A Boat has-a topSpeed, in kilo-meters per hour (kmh)
-	
-	Boat(String manufacturer, String model, int year)
+	private long length; // A Boat has-a length, in meters
+	private long topSpeed; // A Boat has-a topSpeed, in kilo-meters per hour
+							// (kmh)
+
+	Boat(String manufacturer, String model, int year, Person renter)
 	{
-		super(manufacturer, model, year);
+		super(manufacturer, model, year, renter);
 	}
-	
-	Boat(String manufacturer, String model, int year, double length, double topSpeed)
+
+	Boat(String manufacturer, String model, int year, Person renter,
+			long length, long topSpeed)
 	{
-		super(manufacturer, model, year);
+		super(manufacturer, model, year, renter);
 		this.length = length;
 		this.topSpeed = topSpeed;
 	}
-	
+
 	/**
 	 * Return the length, in meters, of the Boat
+	 * 
 	 * @return double
 	 */
-	public double getLength() {
+	public double getLength()
+	{
 		return this.length;
 	}
-	
+
 	/**
 	 * Set the length, in meters, of the Boat
+	 * 
 	 * @param length
 	 */
-	public void setLength(double length) {
+	public void setLength(long length)
+	{
 		this.length = length;
 	}
-	
+
 	/**
 	 * Return the top speed, in kmh, of the Boat
+	 * 
 	 * @return double
 	 */
-	public double getTopSpeed() {
+	public double getTopSpeed()
+	{
 		return this.topSpeed;
 	}
-	
+
 	/**
 	 * Set the top speed, in kmh, of the Boat
+	 * 
 	 * @param speed
 	 */
-	public void setTopSpeed(double speed) {
+	public void setTopSpeed(long speed)
+	{
 		this.topSpeed = speed;
 	}
 }
