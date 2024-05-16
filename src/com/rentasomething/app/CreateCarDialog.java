426525bcdfd861b9ee3775cc8d53f8da1336b7ea
Kaitlyn <118654100+dlw1rm4@users.noreply.github.com>
2024-05-16 16:12:55 -0700

    Can create Car objects now

--- src/com/rentasomething/app/CreateCarDialog.java
+++ src/com/rentasomething/app/CreateCarDialog.java
@@ -1,12 +1,19 @@
 package com.rentasomething.app;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Hashtable;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  * Lead Author(s):
@@ -23,17 +30,92 @@ import javax.swing.SwingConstants;
 
 public class CreateCarDialog extends JDialog
 {
-	public CreateCarDialog() {
+	public CreateCarDialog()
+	{
+		JPanel bottomPanel = new JPanel();
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(5, 2, 5, 18));
+
 		this.setTitle("Create Car");
-		this.setSize(400, 100);
+		this.setSize(400, 300);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width / 2 - this.getSize().width / 2,
 				dim.height / 2 - this.getSize().height / 2);
-		JLabel jl = new JLabel("Car creation");
-		jl.setHorizontalAlignment(SwingConstants.CENTER);
-		this.add(jl);
+		this.setLayout(new BorderLayout());
+
+		JLabel rnLabel = new JLabel("   Renter Name:");
+		JComboBox rnComboBox = new JComboBox(Database.namesArray());
+		centerPanel.add(rnLabel);
+		centerPanel.add(rnComboBox);
+
+		JLabel maLabel = new JLabel("   Manufacturer:");
+		JTextField maTextField = new JTextField();
+		centerPanel.add(maLabel);
+		centerPanel.add(maTextField);
+
+		JLabel moLabel = new JLabel("   Model:");
+		JTextField moTextField = new JTextField();
+		centerPanel.add(moLabel);
+		centerPanel.add(moTextField);
+
+		JLabel yLabel = new JLabel("   Year:");
+		JTextField yTextField = new JTextField();
+		centerPanel.add(yLabel);
+		centerPanel.add(yTextField);
+
+		JLabel miLabel = new JLabel("   Mileage:");
+		JTextField miTextField = new JTextField();
+		centerPanel.add(miLabel);
+		centerPanel.add(miTextField);
+
+		JButton createButton = new JButton("Create");
+		bottomPanel.add(createButton);
+
+		this.add(bottomPanel, BorderLayout.SOUTH);
+		this.add(centerPanel, BorderLayout.CENTER);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+		createButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				if (maTextField.getText().isEmpty()
+						|| moTextField.getText().isEmpty()
+						|| yTextField.getText().isEmpty()
+						|| miTextField.getText().isEmpty())
+				{
+					new ErrorDialog("Fields cannot be empty");
+				}
+				else if (ErrorDialog
+						.containsLetters(maTextField.getText()) == true
+						|| ErrorDialog
+								.containsLetters(moTextField.getText()) == true
+						|| ErrorDialog
+								.containsLetters(yTextField.getText()) == true
+						|| ErrorDialog
+								.isDecimal(miTextField.getText()) == false)
+				{
+					new ErrorDialog(
+							"Field 1, 2, must be Strings. Fields 3 must be an Integer. Fields 4 must be numbers.");
+				}
+				else
+				{
+					// TODO: Properly validate that SSN is only numbers, and if
+					// not, throw error pop up
+					Hashtable<String, Person> personIdentification = Database
+							.getNames();
+					Car car = new Car(maTextField.getText(),
+							moTextField.getText(),
+							Integer.parseInt(yTextField.getText()),
+							personIdentification
+									.get(rnComboBox.getSelectedItem()),
+							Long.parseLong(miTextField.getText()));
+				}
+			}
+		});
+
 	}
 
 }
